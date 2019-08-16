d58a250ca51259b354ba61efc3cca951e2ff2ee9
理想国 <364112371@qq.com>
2019-08-16 20:46:33 +0800

    加入DBUtil逻辑

--- src/itat/zttc/shop/dao/UserDao.java
+++ src/itat/zttc/shop/dao/UserDao.java
@@ -1,27 +1,52 @@
 package itat.zttc.shop.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
-import itat.zttc.shop.model.User;
+import com.mysql.jdbc.Connection;
 
-public class UserDao implements IUserDao{
+import itat.zttc.shop.model.ShopException;
+import itat.zttc.shop.model.User;
+import itat.zttc.shop.util.DBUtil;
+
+public class UserDao implements IUserDao {
 
 	@Override
 	public void add(User user) {
-		// TODO Auto-generated method stub
-		
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			con = DBUtil.getConnection();
+			String sql = "select count(*) from t_user where username=?";
+			ps = con.prepareStatement(sql);
+			ps.setString(1, user.getUsername());
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				if (rs.getInt(1) > 0)
+					throw new ShopException("添加的用户已经存在，不能继续添加");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBUtil.close(con);
+			DBUtil.close(ps);
+			DBUtil.close(rs);
+		}
 	}
 
 	@Override
 	public void delete(int id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(User user) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
--- src/itat/zttc/shop/util/DBUtil.java
+++ src/itat/zttc/shop/util/DBUtil.java
@@ -1,5 +1,50 @@
 package itat.zttc.shop.util;
 
-public class DBUtil {
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import com.mysql.jdbc.Connection;
+
+public class DBUtil {
+	public static Connection getConnection() {
+		String username = "root";
+		String password = "123456";
+		String url = "jdbc:mysql://localhost:3306/itat_shop";
+		Connection con = null;
+		try {
+			con = (Connection) DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	public static void close(Connection con) {
+		try {
+			if (con != null)
+				con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void close(PreparedStatement ps) {
+		try {
+			if (ps != null)
+				ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void close(ResultSet rs) {
+		try {
+			if (rs != null)
+				rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
